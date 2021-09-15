db762ebaba53996211030c23dac4c3aeabe95b0e
rf-castle <kouta.tashiro.2000@gmail.com>
2021-09-16 08:17:14 +0900

    add忘れ

---
+++ app/handler/accounts/fetch.go
@@ -0,0 +1,24 @@
+package accounts
+
+import (
+	"encoding/json"
+	"github.com/go-chi/chi"
+	"net/http"
+	"yatter-backend-go/app/handler/httperror"
+)
+
+// Handle request for `GET /v1/accounts/{username}`
+func (h *handler) Fetch(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	username := chi.URLParam(r, "username")
+	repoAccount := h.app.Dao.Account()
+	account, err := repoAccount.FindByUsername(ctx, username)
+	if err != nil {
+		httperror.InternalServerError(w, err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(account); err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+}
